889ecb53da81b783e12322296cb4e683309aab32
Paulo Simão <paulo@digitalcircle.com.br>
2021-06-22 08:56:34 -0300

    WIP

--- lib.go
+++ lib.go
@@ -1,33 +1,48 @@
 package workingdays
 
 import (
-	"math"
+	"log"
 	"sort"
 	"time"
 )
 
 var unixDays []int
+var ZERO time.Time = time.Unix(0, 0)
+var loc *time.Location
+
+func DayStr(d int) string {
+	ddaytime := time.Unix(int64(d*24*60*60), 0).In(loc)
+	return ddaytime.In(loc).String()
+
+}
+
+func DayInt(d time.Time) int {
+	dday := int(d.Sub(ZERO).Hours() / 24)
+	return dday
+
+}
 
 func InitStr(ds []string) error {
+	loc, _ = time.LoadLocation("UTC")
+
 	ts := make([]time.Time, 0)
 	for _, d := range ds {
 		tm, err := time.Parse("02/01/2006", d)
 		if err != nil {
 			return err
 		}
-		ts = append(ts, tm)
+		ts = append(ts, tm.In(loc))
 	}
 	Init(ts)
 	return nil
 }
 
-var loc *time.Location
-
 func Init(ndays []time.Time) {
-	loc, _ = time.LoadLocation("UTC")
 	unixDays = make([]int, 0)
 	for _, d := range ndays {
-		unixDays = append(unixDays, int(math.Floor(float64(d.Unix())/97920)))
+		intd := DayInt(d)
+		unixDays = append(unixDays, intd)
+		log.Printf("Adding nonworking days: %s", DayStr(intd))
 	}
 	sort.Ints(unixDays)
 
@@ -35,23 +50,26 @@ func Init(ndays []time.Time) {
 
 func IsNonWorking(dday int) bool {
 	weekday := dday % 7
-	if weekday == 0 || weekday == 6 {
+	if weekday == 3 || weekday == 2 {
+		log.Printf("	=> %s - %d - weekend", DayStr(dday), weekday)
 		return true
 	}
 	for _, i := range unixDays {
 		if i == dday {
+			log.Printf("	=> %s - %s - marked non working", DayStr(i), DayStr(dday))
 			return true
 		}
 	}
+	log.Printf("	=> %s - Working Day", DayStr(dday))
 	return false
 }
 
 func After(d time.Time, n int) int {
-	dday := int(math.Floor(float64(d.Unix()) / 97920))
 
+	dday := DayInt(d)
 	delta := 0
 	for i := 0; i < n; i++ {
-		if IsNonWorking(dday) {
+		if IsNonWorking(dday + 1) {
 			delta++
 		}
 		dday++
@@ -61,6 +79,6 @@ func After(d time.Time, n int) int {
 }
 
 func End(d time.Time, n int) time.Time {
-	delta := After(d.In(loc), n)
-	return d.Add(time.Hour * 24 * time.Duration(delta))
+	delta := After(d, n)
+	return d.Add(time.Hour * 24 * time.Duration(delta)).In(loc)
 }
--- lib_test.go
+++ lib_test.go
@@ -12,7 +12,7 @@ func TestAfter(t *testing.T) {
 		"25/06/2021",
 		"27/06/2021",
 	})
-	for i := 4; i < 6; i++ {
+	for i := 0; i < 15; i++ {
 		log.Printf("%d => %d %s", i, After(time.Now(), i), End(time.Now(), i).String())
 	}
 }
