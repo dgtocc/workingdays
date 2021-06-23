83abae2c3e952e90c49f637a27b3ccd9da3c9cfd
Paulo Simão <paulo@digitalcircle.com.br>
2021-06-22 21:46:44 -0300

    WIP

--- lib.go
+++ lib.go
@@ -12,73 +12,92 @@ var loc *time.Location
 
 func DayStr(d int) string {
 	ddaytime := time.Unix(int64(d*24*60*60), 0).In(loc)
-	return ddaytime.In(loc).String()
+	return ddaytime.String()
 
 }
 
+//Retorna o numero do dia a contar de ZERO, sendo ZERO 01/Jan/1970
 func DayInt(d time.Time) int {
-	dday := int(d.Sub(ZERO).Hours() / 24)
+	d, _ = time.Parse("02/01/06", d.In(loc).Format("02/01/06"))
+	dday := int(d.In(loc).Sub(ZERO).Hours() / 24)
+	//log.Printf("Dayint: %s => %d", d.String(), dday)
 	return dday
-
 }
 
-func InitStr(ds []string) error {
+//Inicializa função com strings
+func InitStr(ds []string, format string) error {
 	loc, _ = time.LoadLocation("UTC")
 
 	ts := make([]time.Time, 0)
 	for _, d := range ds {
-		tm, err := time.Parse("02/01/2006", d)
+		tm, err := time.Parse(format, d)
 		if err != nil {
 			return err
 		}
-		ts = append(ts, tm.In(loc))
+		ts = append(ts, tm)
 	}
 	Init(ts)
 	return nil
 }
 
+//Inicializa função com Array de de time.Time
 func Init(ndays []time.Time) {
 	unixDays = make([]int, 0)
 	for _, d := range ndays {
 		intd := DayInt(d)
 		unixDays = append(unixDays, intd)
-		log.Printf("Adding nonworking days: %s", DayStr(intd))
+		//log.Printf("Adding nonworking days: %s", DayStr(intd))
 	}
 	sort.Ints(unixDays)
 
 }
 
+//Verifica se o dia é util ou não
+//Pode ser otimizada, se as consultas forem sempre crescentes através
+//de um memento[GoF]
 func IsNonWorking(dday int) bool {
 	weekday := dday % 7
 	if weekday == 3 || weekday == 2 {
-		log.Printf("	=> %s - %d - weekend", DayStr(dday), weekday)
+		log.Printf("		=> %s(%d) - %d - weekend", DayStr(dday), dday, weekday)
 		return true
 	}
 	for _, i := range unixDays {
 		if i == dday {
-			log.Printf("	=> %s - %s - marked non working", DayStr(i), DayStr(dday))
+			log.Printf("		=> %s - %s (%d)- marked non working", DayStr(i), DayStr(dday), dday)
 			return true
 		}
+		if i > dday {
+			log.Printf("		=> %s (%d) - Working Day", DayStr(dday), dday)
+			return false
+		}
 	}
-	log.Printf("	=> %s - Working Day", DayStr(dday))
+	log.Printf("		=> %s (%d) - Working Day", DayStr(dday), dday)
 	return false
 }
 
+//Dado data e no dias uteis, retornar no dias corridos
 func After(d time.Time, n int) int {
 
 	dday := DayInt(d)
 	delta := 0
-	for i := 0; i < n; i++ {
-		if IsNonWorking(dday + 1) {
-			delta++
+	//if n==0{
+	//	return 0
+	//}
+	i := 0
+	for n > 0 {
+		//log.Printf("==> Evaluating: %s", DayStr(dday+i))
+		if !IsNonWorking(dday + i) {
+			n--
 		}
-		dday++
+		i++
 	}
-	delta = delta + n
+
+	delta = delta + i
 	return delta
 }
 
+//Dada data inicio e no dias uteis, calcula data Fim
 func End(d time.Time, n int) time.Time {
 	delta := After(d, n)
-	return d.Add(time.Hour * 24 * time.Duration(delta)).In(loc)
+	return d.Add(time.Hour * 24 * time.Duration(delta))
 }
--- lib_test.go
+++ lib_test.go
@@ -8,11 +8,18 @@ import (
 
 func TestAfter(t *testing.T) {
 	InitStr([]string{
-		"23/06/2021",
-		"25/06/2021",
-		"27/06/2021",
-	})
-	for i := 0; i < 15; i++ {
-		log.Printf("%d => %d %s", i, After(time.Now(), i), End(time.Now(), i).String())
+		"23/06/21",
+		"25/06/21",
+		"27/06/21",
+		"29/06/21",
+		"06/07/21",
+		"12/07/21",
+		"08/07/21",
+		"15/07/21",
+	}, "02/01/06")
+	for i := 0; i < 16; i++ {
+		//i:=7
+		log.Printf("===========")
+		log.Printf("%d => %d %s", i, After(time.Now(), i), End(time.Now(), i).Format("02/01/06"))
 	}
 }
